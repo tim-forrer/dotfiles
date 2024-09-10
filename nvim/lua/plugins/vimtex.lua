75e838c43236bf269ccccf7f774c857180732715
tim@mbp <timforrer16@gmail.com>
2024-09-10 16:09:25 +0900

    update dotfiles for macos env

--- nvim/lua/plugins/vimtex.lua
+++ nvim/lua/plugins/vimtex.lua
@@ -3,9 +3,9 @@ M.lazy_config = {
   "lervag/vimtex",
   lazy = false,
 }
-M.load = function ()
-  -- vim.g.vimtex_view_method = "skim" -- on MacOS
-  vim.g.vimtex_view_method = "zathura" -- on Linux
+M.load = function()
+  vim.g.vimtex_view_method = "skim" -- on MacOS
+  -- vim.g.vimtex_view_method = "zathura" -- on Linux
   vim.g.vimtex_quickfix_open_on_warning = 0
 end
 return M
