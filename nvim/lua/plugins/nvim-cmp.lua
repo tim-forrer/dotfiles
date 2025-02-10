2cfaea42d347e36ad2a29b0faa4f4c7f7a0ecf98
tim@mbp <timforrer16@gmail.com>
2025-02-10 10:48:44 +0900

    update completions, os dependent vimtex

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -22,8 +22,8 @@ return {
         },
         sources = cmp.config.sources({
           { name = 'nvim_lsp' },
-        }, {
           { name = 'buffer' },
+          { name = 'path' },
         })
       })
 
--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -7,11 +7,42 @@ return {
   },
   config = function()
     local lspconfig = require("lspconfig")
+    local cmp_nvim_lsp = require("cmp_nvim_lsp")
+    local capabilities = cmp_nvim_lsp.default_capabilities()
+
     require("mason-lspconfig").setup_handlers({
       -- Default handler for all servers
       function(server_name)
         lspconfig[server_name].setup({})
       end,
+    })
+
+    lspconfig.pyright.setup({
+      capabilities = capabilities,
+      settings = {
+        python = {
+          analysis = {
+            autoSearchPaths = true,
+            useLibraryCodeForTypes = true,
+          },
+        },
+      },
+    })
+
+    -- Configure Lua Language Server (`lua_ls`) to recognize `vim` global
+    lspconfig.lua_ls.setup({
+      settings = {
+        Lua = {
+          diagnostics = {
+            globals = { "vim" }, -- Allow `vim` global to prevent warnings
+          },
+          workspace = {
+            library = vim.api.nvim_get_runtime_file("", true), -- Make LSP aware of Neovim runtime
+            checkThirdParty = false, -- Disable third-party checks if unnecessary
+          },
+          telemetry = { enable = false }, -- Disable telemetry
+        },
+      },
     })
   end,
 }
