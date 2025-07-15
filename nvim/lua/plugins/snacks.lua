c8ef2502d02c5b8a1e3907b69d01be7810af6e79
tim@mbp <timforrer16@gmail.com>
2025-07-15 22:36:06 +0900

    :recycle: Finish refactor️

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -2,23 +2,7 @@ local snacks_config = require("snacks_config")
 
 return {
 	"folke/snacks.nvim",
-	---@type snacks.Config
-	opts = {
-		picker = { enabled = false },
-		image = {
-			doc = {
-				enabled = true,
-				inline = false,
-				float = true,
-				max_width = 80,
-				max_height = 40,
-			},
-			math = { enabled = false },
-		},
-		indent = { enabled = true },
-		lazygit = { enabled = true },
-		input = { enabled = true },
-	},
+	opts = snacks_config.opts,
 	lazy = false,
 	priority = 1000,
 	keys = snacks_config.keys,
--- nvim/lua/snacks_config.lua
+++ nvim/lua/snacks_config.lua
@@ -1,5 +1,11 @@
 local M = {}
 
+M.module_list = {
+	"image",
+	"lazygit",
+	"indent",
+}
+
 M.keys = {
 	{
 		"<leader>lg",
@@ -9,5 +15,22 @@ M.keys = {
 		desc = "Open Lazygit",
 	},
 }
+
+M.opts = {}
+M.opts.image = {
+	math = { enabled = false },
+	doc = {
+		enabled = true,
+		float = true,
+		inline = false,
+		max_width = 40,
+		max_height = 40,
+	},
+}
+for _, module_name in ipairs(M.module_list) do
+	if M.opts[module_name] == nil then
+		M.opts[module_name] = { enabled = true }
+	end
+end
 
 return M
