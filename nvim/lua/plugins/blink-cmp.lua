4393a4a1f35cc413d2a1b1e4482b50b9ac7ec1ef
tim@mbp <timforrer16@gmail.com>
2025-07-16 20:24:27 +0900

    :bug: Fix completions per file, terminal

--- nvim/lua/plugins/blink-cmp.lua
+++ nvim/lua/plugins/blink-cmp.lua
@@ -1,3 +1,44 @@
+local cmp_sources = {
+	providers = {
+		emoji = {
+			name = "Emoji",
+			module = "blink-emoji",
+			score_offset = 15,
+			opts = {
+				insert = true,
+				trigger = function()
+					return { ":" }
+				end,
+			},
+		},
+		lazydev = {
+			name = "LazyDev",
+			module = "lazydev.integrations.blink",
+			score_offset = 100,
+		},
+		nerdfont = {
+			name = "Nerd Fonts",
+			module = "blink-nerdfont",
+			score_offset = 10,
+			opts = { insert = true },
+		},
+	},
+}
+
+cmp_sources.default = { "lsp", "path" }
+cmp_sources.per_filetype = {
+	markdown = { "path", "emoji", "nerdfont" },
+	lua = { "lsp", "path", "lazydev" },
+}
+
+local completion = {
+	documentation = { auto_show = true },
+}
+completion.list = {
+	max_items = 20,
+	selection = { preselect = false },
+}
+
 return {
 	"saghen/blink.cmp",
 	dependencies = {
@@ -10,76 +51,21 @@ return {
 	---@module 'blink.cmp'
 	---@type blink.cmp.Config
 	opts = {
-		-- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-		-- 'super-tab' for mappings similar to vscode (tab to accept)
-		-- 'enter' for enter to accept
-		-- 'none' for no mappings
-		--
-		-- All presets have the following mappings:
-		-- C-space: Open menu or open docs if already open
-		-- C-n/C-p or Up/Down: Select next/previous item
-		-- C-e: Hide menu
-		-- C-k: Toggle signature help (if signature.enabled = true)
-		--
-		-- See :h blink-cmp-config-keymap for defining your own keymap
 		keymap = { preset = "default" },
 
 		appearance = {
-			-- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-			-- Adjusts spacing to ensure icons are aligned
 			nerd_font_variant = "normal",
 		},
 
 		-- (Default) Only show the documentation popup when manually triggered
-		completion = { documentation = { auto_show = false } },
-
-		-- Default list of enabled providers defined so that you can extend it
-		-- elsewhere in your config, without redefining it, due to `opts_extend`
-		sources = {
-			-- https://cmp.saghen.dev/recipes.html#dynamically-picking-providers-by-treesitter-node-filetype
-			default = function()
-				local cmp_table = { "lsp", "path", "buffer" }
-				local filetype = vim.bo.filetype
-				if filetype == "lua" then
-					table.insert(cmp_table, "lazydev")
-				elseif filetype == "markdown" then
-					cmp_table = { "buffer", "emoji", "nerdfont" }
-				end
-				return cmp_table
-			end,
-			providers = {
-				emoji = {
-					name = "Emoji",
-					module = "blink-emoji",
-					score_offset = 15,
-					opts = {
-						insert = true, -- Insert emoji (default) or complete its name
-						---@type string|table|fun():table
-						trigger = function()
-							return { ":" }
-						end,
-					},
-				},
-				lazydev = {
-					name = "LazyDev",
-					module = "lazydev.integrations.blink",
-					score_offset = 100,
-				},
-				nerdfont = {
-					name = "Nerd Fonts",
-					module = "blink-nerdfont",
-					score_offset = 10,
-					opts = { insert = true },
-				},
-			},
-		},
+		completion = completion,
+		sources = cmp_sources,
 		term = {
 			enabled = true,
 			keymap = { preset = "inherit" },
 			sources = { "emoji", "path" },
 		},
-
-		fuzzy = { implementation = "prefer_rust_with_warning" },
 	},
+
 	opts_extend = { "sources.default" },
 }
